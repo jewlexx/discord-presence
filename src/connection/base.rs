f07c9f12685dcc432af8f200e6a3182fbb38b5b8
Patrick Auernig <patrick.auernig@mykolab.com>
2018-12-06 17:15:47 +0100

    Make ping command blocking

--- src/connection/base.rs
+++ src/connection/base.rs
@@ -18,7 +18,7 @@ macro_rules! try_until_done {
     [ $e:expr ] => {
         loop {
             match $e {
-                Ok(_) => break,
+                Ok(v) => break v,
                 Err(Error::IoError(ref err)) if err.kind() == ErrorKind::WouldBlock => (),
                 Err(why) => return Err(why),
             }
@@ -32,16 +32,22 @@ macro_rules! try_until_done {
 pub trait Connection: Sized {
     type Socket: Write + Read;
 
+    /// The internally stored socket connection.
     fn socket(&mut self) -> &mut Self::Socket;
 
+    /// The base path were the socket is located.
     fn ipc_path() -> PathBuf;
 
+    /// Establish a new connection to the server.
     fn connect() -> Result<Self>;
 
+    /// The full socket path.
     fn socket_path(n: u8) -> PathBuf {
         Self::ipc_path().join(format!("discord-ipc-{}", n))
     }
 
+    /// Perform a handshake on this socket connection.
+    /// Will block until complete.
     fn handshake(&mut self, client_id: u64) -> Result<()> {
         let hs = json![{
             "client_id": client_id.to_string(),
@@ -55,13 +61,16 @@ pub trait Connection: Sized {
         Ok(())
     }
 
+    /// Ping the server and get a pong response.
+    /// Will block until complete.
     fn ping(&mut self) -> Result<OpCode> {
         let message = Message::new(OpCode::Ping, json![{}]);
-        self.send(message)?;
-        let response = self.recv()?;
+        try_until_done!(self.send(message.clone()));
+        let response = try_until_done!(self.recv());
         Ok(response.opcode)
     }
 
+    /// Send a message to the server.
     fn send(&mut self, message: Message) -> Result<()> {
         match message.encode() {
             Err(why) => error!("{:?}", why),
@@ -73,6 +82,7 @@ pub trait Connection: Sized {
         Ok(())
     }
 
+    /// Receive a message from the server.
     fn recv(&mut self) -> Result<Message> {
         let mut buf = BytesMut::new();
         buf.resize(1024, 0);
--- src/models/message.rs
+++ src/models/message.rs
@@ -30,7 +30,7 @@ impl OpCode {
     }
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct Message {
     pub opcode: OpCode,
     pub payload: String,
