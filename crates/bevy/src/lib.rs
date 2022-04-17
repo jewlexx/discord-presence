a09845e6bfde3bb608cd57b3142b18387543323d
Juliette Cordor <grassojames5@gmail.com>
2022-04-17 11:58:39 +1000

    fixes

--- crates/bevy/src/lib.rs
+++ crates/bevy/src/lib.rs
@@ -1,8 +1,33 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use std::sync::{Arc, Mutex};
+
+use bevy::prelude::*;
+use discord_presence::*;
+use serde_json::Value;
+
+pub struct RPCConfig {
+    pub client_id: u64,
+    pub show_time: bool,
+}
+
+pub struct RPCPlugin(RPCConfig);
+
+impl Plugin for RPCPlugin {
+    fn build(&self, app: &mut App) {
+        let is_ready = Arc::new(Mutex::new(false));
+        let error = Arc::new(Mutex::<Option<Value>>::new(None));
+        let client_config = &self.0;
+        let mut client = Client::new(client_config.client_id);
+
+        client.on_ready(move |_| {
+            let is_ready = Arc::clone(&is_ready);
+            *is_ready.lock().unwrap() = true;
+        });
+
+        client.on_error(move |e| {
+            let error = Arc::clone(&error);
+            *error.lock().unwrap() = Some(e.event);
+        });
+
+        app.add_state("DiscordRPC");
     }
 }
