97f575c6cd734e37ad411ec5653135fb29260f7a
Juliette Cordor <grassojames5@gmail.com>
2022-04-22 10:35:15 +1000

    added payload docs

--- crates/rpc/src/models/payload.rs
+++ crates/rpc/src/models/payload.rs
@@ -2,22 +2,28 @@ use super::{Command, Event, Message};
 use crate::utils;
 use serde::{de::DeserializeOwned, Serialize};
 
+/// The Discord client payload
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
 pub struct Payload<T>
 where
     T: Serialize,
 {
+    /// The payload command
     pub cmd: Command,
 
+    /// The payload args
     #[serde(skip_serializing_if = "Option::is_none")]
     pub args: Option<T>,
 
+    /// The payload data
     #[serde(skip_serializing_if = "Option::is_none")]
     pub data: Option<T>,
 
+    /// The payload event
     #[serde(skip_serializing_if = "Option::is_none")]
     pub evt: Option<Event>,
 
+    /// The payload nonce
     #[serde(skip_serializing_if = "Option::is_none")]
     pub nonce: Option<String>,
 }
@@ -26,6 +32,7 @@ impl<T> Payload<T>
 where
     T: Serialize,
 {
+    /// Create a `Payload`, by generating a nonce
     pub fn with_nonce(cmd: Command, args: Option<T>, data: Option<T>, evt: Option<Event>) -> Self {
         Self {
             cmd,
