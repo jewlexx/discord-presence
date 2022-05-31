6d703e8c7f58c333d8996eaf3c4a7e435ea773ea
Juliette Cordor <grassojames5@gmail.com>
2022-05-31 12:18:33 +1000

    improved cfg-if syntax

--- crates/rpc/src/connection/mod.rs
+++ crates/rpc/src/connection/mod.rs
@@ -1,12 +1,15 @@
 mod base;
 mod manager;
-#[cfg(unix)]
-mod unix;
-#[cfg(windows)]
-mod windows;
 
-#[cfg(unix)]
-pub use self::unix::UnixConnection as SocketConnection;
-#[cfg(windows)]
-pub use self::windows::WindowsConnection as SocketConnection;
-pub use self::{base::Connection, manager::Manager};
+pub use base::Connection;
+pub use manager::Manager;
+
+cfg_if::cfg_if! {
+    if #[cfg(unix)] {
+        mod unix;
+        pub use unix::UnixConnection as SocketConnection;
+    } else if #[cfg(windows)] {
+        mod windows;
+        pub use windows::WindowsConnection as SocketConnection;
+    }
+}
