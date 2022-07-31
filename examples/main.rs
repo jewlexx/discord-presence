7b164675ea65391fc5d003619a26831c1a8193ac
Juliette Cordor <grassojames5@gmail.com>
2022-08-01 09:53:54 +1000

    moved demo to custom name

--- examples/main.rs
+++ examples/main.rs
@@ -2,7 +2,7 @@ use discord_presence::Client as DiscordRPC;
 use std::{thread, time};
 
 fn main() {
-    let mut drpc = DiscordRPC::new(425407036495495169);
+    let mut drpc = DiscordRPC::new(1003450375732482138);
 
     drpc.on_ready(|_ctx| {
         println!("ready?");
