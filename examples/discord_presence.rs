62d8c4e34ae71755c821770e5067f90410aca479
Juliette Cordor <juliette@jewelexx.com>
2023-08-31 11:33:41 +1000

    updated discord_presence example

---
+++ examples/discord_presence.rs
@@ -0,0 +1,44 @@
+use discord_presence::Client;
+
+fn main() {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::TRACE)
+        .init();
+
+    let mut drpc = Client::new(1003450375732482138);
+
+    drpc.on_ready(|_ctx| {
+        println!("READY!");
+    });
+
+    drpc.on_error(|ctx| {
+        eprintln!("An error occured, {}", ctx.event);
+    });
+
+    drpc.start();
+
+    loop {
+        let mut buf = String::new();
+
+        io::stdin().read_line(&mut buf).unwrap();
+        buf.pop();
+
+        if buf.is_empty() {
+            if let Err(why) = drpc.clear_activity() {
+                println!("Failed to clear presence: {}", why);
+            }
+        } else {
+            if let Err(why) = drpc.set_activity(|a| {
+                a.state("Running examples").assets(|ass| {
+                    ass.large_image("ferris_wat")
+                        .large_text("wat.")
+                        .small_image("rusting")
+                        .small_text("rusting...")
+                })
+            }) {
+                println!("Failed to set presence: {}", why);
+            }
+        }
+    }
+    // drpc.block_on().unwrap();
+}
