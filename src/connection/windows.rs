3b31319da2863282c277ab0ddf90166265aef8ec
vernoxvernax <vernoxvernax@gmail.com>
2025-03-22 18:01:30 +0100

    fix(windows): replace named_pipe with File for socket type; fix imports

--- src/connection/windows.rs
+++ src/connection/windows.rs
@@ -1,21 +1,25 @@
-use std::path::PathBuf;
-
-use named_pipe::PipeClient;
+use std::{
+    fs::{File, OpenOptions},
+    io::{ErrorKind, Read},
+    os::windows::fs::OpenOptionsExt,
+    path::PathBuf,
+};
 
 use super::base::Connection;
-use crate::Result;
+use crate::{DiscordError, Result};
 
 pub struct Socket {
-    socket: PipeClient,
+    socket: File,
 }
 
 impl Connection for Socket {
-    type Socket = PipeClient;
+    type Socket = File;
 
     fn connect() -> Result<Self> {
-        let mut socket = PipeClient::connect(Self::socket_path(0))?;
-        socket.set_read_timeout(Some(Self::READ_WRITE_TIMEOUT));
-        socket.set_write_timeout(Some(Self::READ_WRITE_TIMEOUT));
+        let path = Self::socket_path(0);
+
+        let socket = OpenOptions::new().access_mode(0x3).open(&path)?;
+
         Ok(Self { socket })
     }
 
@@ -29,7 +33,7 @@ impl Connection for Socket {
 
     fn try_read(&mut self, buf: &mut [u8]) -> Result<usize> {
         if self.socket().metadata()?.len() == 0 {
-            return Err(DiscordError::IoError(Error::new(
+            return Err(DiscordError::IoError(std::io::Error::new(
                 ErrorKind::WouldBlock,
                 "No data available",
             )));
